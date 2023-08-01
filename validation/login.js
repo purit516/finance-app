0f5df7e07564e090288392a302208933737b6363
Purit <pb465@cornell.edu>
2023-08-01 18:41:35 -0400

    initialize server and login

---
+++ validation/login.js
@@ -0,0 +1,23 @@
+import { isEmpty as _isEmpty, isEmail } from "validator";
+import isEmpty from "is-empty";
+
+export default function validateLoginInput(data) {
+  let errors = {};
+  // Convert empty fields to an empty string so we can use validator functions
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  // Email checks
+  if (_isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  // Password checks
+  if (_isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+}
