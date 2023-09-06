5d1eaef99ffe0dfc1fbdffe1fb33ef8c6c3b3e6c
Purit <pb465@cornell.edu>
2023-09-05 23:10:26 -0400

    fix errors

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), thunk],
